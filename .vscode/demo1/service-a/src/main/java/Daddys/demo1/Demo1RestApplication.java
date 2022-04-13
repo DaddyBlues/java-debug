1482b8e933a48a0f2f0af318c6de350608e76f87
DaddyBlues <102938237+DaddyBlues@users.noreply.github.com>
2022-04-13 23:23:52 +0800

    commit

---
+++ .vscode/demo1/service-a/src/main/java/Daddys/demo1/Demo1RestApplication.java
@@ -0,0 +1,11 @@
+package Daddys.demo1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class Demo1RestApplication extends Application {
+}
